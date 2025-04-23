af77b26103685290bbb325263f799e045e83f03d
Yi Yang <sacriley@gmail.com>
2025-04-23 18:14:08 +0800

    update

---
+++ src/Core/NovaToolsProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace FF\NovaTools\Core;
+
+use Illuminate\Support\ServiceProvider;
+
+class NovaToolsProvider extends ServiceProvider
+{
+
+    public static string $name = 'nova-tools';
+
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+
+}
